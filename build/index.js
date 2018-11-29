21b91d915908d55c31f3486f19ca11f9dfa4839d
dominic.griesel <d.griesel@gmx.net>
2018-11-29 16:36:43 +0100

    Work on interview system

--- build/index.js
+++ build/index.js
@@ -11,18 +11,45 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 // tslint:disable-next-line:variable-name
 const enquirer_1 = require("enquirer");
+const questions = [
+    {
+        type: "select",
+        name: "language",
+        message: "Mit welcher Sprache soll der Adapter programmiert werden?",
+        choices: [
+            "JavaScript",
+            "TypeScript",
+        ],
+    },
+    {
+        type: "confirm",
+        name: "really",
+        message: "Wirklich?",
+        condition: { name: "language", value: "JavaScript" },
+        action: (val) => {
+            if (val) {
+                console.log("Du bist unbelehrbar!");
+            }
+            else {
+                console.log("gut so!");
+            }
+            return Promise.resolve(!val);
+        },
+    },
+];
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        const language = yield enquirer_1.prompt([{
-                type: "select",
-                name: "language",
-                message: "Mit welcher Sprache soll der Adapter programmiert werden?",
-                choices: [
-                    "JavaScript",
-                    "TypeScript",
-                ],
-            }]);
-        console.dir(language);
+        let answers = {};
+        for (const q of questions) {
+            if (q.condition == undefined || answers[q.condition.name] === q.condition.value) {
+                const answer = yield enquirer_1.prompt([q]);
+                if (q.action && !(yield q.action(answer[q.name]))) {
+                    process.exit(1);
+                }
+                answers = Object.assign({}, answers, answer);
+            }
+        }
+        console.dir(answers);
     });
 }
 main().catch(console.error);
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,18 @@
 // tslint:disable-next-line:variable-name
 import { prompt } from "enquirer";
 
-async function main() {
-	const language = await prompt([{
+// Sadly, Enquirer does not export the PromptOptions type
+// tslint:disable-next-line:ban-types
+type PromptOptions = Exclude<Parameters<typeof prompt>[0], Function | any[]>;
+type QuestionAction<T> = (value: T) => Promise<boolean>;
+interface QuestionMeta {
+	condition?: {name: string, value: string | boolean | number};
+	action?: QuestionAction<string | boolean | number>;
+}
+type Question = PromptOptions & QuestionMeta;
+
+const questions: Question[] = [
+	{
 		type: "select",
 		name: "language",
 		message: "Mit welcher Sprache soll der Adapter programmiert werden?",
@@ -12,7 +22,34 @@ async function main() {
 			"JavaScript",
 			"TypeScript",
 		],
-	}]);
-	console.dir(language);
+	},
+	{
+		type: "confirm",
+		name: "really",
+		message: "Wirklich?",
+		condition: {name: "language", value: "JavaScript"},
+		action: (val: boolean) => {
+			if (val) {
+				console.log("Du bist unbelehrbar!");
+			} else {
+				console.log("gut so!");
+			}
+			return Promise.resolve(!val);
+		},
+	},
+];
+
+async function main() {
+	let answers: Record<string, any> = {};
+	for (const q of questions) {
+		if (q.condition == undefined || answers[q.condition.name] === q.condition.value) {
+			const answer: Record<string, any> = await prompt([q]);
+			if (q.action && !await q.action(answer[q.name as string])) {
+				process.exit(1);
+			}
+			answers = {...answers, ...answer};
+		}
+	}
+	console.dir(answers);
 }
 main().catch(console.error);
