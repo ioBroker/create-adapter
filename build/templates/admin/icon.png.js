071e3870e72bbfa547bc96c5ba80f4a14edad68b
dominic.griesel <d.griesel@gmx.net>
2018-12-14 15:47:56 +0100

    Fix icon path

--- build/templates/admin/icon.png.js
+++ build/templates/admin/icon.png.js
@@ -1,7 +1,7 @@
 "use strict";
 const fs = require("fs-extra");
 const path = require("path");
-const templateFunction = () => fs.readFile(path.resolve("../../../adapter-creator.png"));
+const templateFunction = () => fs.readFile(path.join(__dirname, "../../../adapter-creator.png"));
 templateFunction.customPath = answers => `admin/${answers.adapterName}.png`;
 templateFunction.noReformat = true; // Don't format binary files
 module.exports = templateFunction;
