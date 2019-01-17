6e1113b35859893579559fae75a33f0605cc4bba
Dominic Griesel <d.griesel@gmx.net>
2019-01-17 14:57:19 +0100

    Revive PR #60

--- build/templates/widgets/template.js.js
+++ build/templates/widgets/template.js.js
@@ -15,85 +15,10 @@ const templateFunction = answers => {
 "use strict";
 
 // add translations for edit mode
-if (vis.editMode) {
-	$.extend(true, systemDictionary, {
-		"myColor": {
-			"en": "myColor",
-			"de": "meineColor",
-			"ru": "Мой цвет",
-			"pt": "minhaCor",
-			"nl": "mijnKleur",
-			"fr": "maCouleur",
-			"it": "mioColore",
-			"es": "miColor",
-			"pl": "mójKolor",
-			"zh-cn": "我的颜色"
-		},
-		"myColor_tooltip": {
-			"en": "Description of\\x0AmyColor",
-			"de": "Beschreibung von\\x0AmyColor",
-			"ru": "Описание\\x0AmyColor",
-			"pt": "Descrição de\\x0AmyColor",
-			"nl": "Beschrijving van\\x0AmyColor",
-			"fr": "Description de\\x0AmyColor",
-			"it": "Descrizione di\\x0AmyColor",
-			"es": "Descripción de\\x0AmyColor",
-			"pl": "Opis\\x0AmyColor",
-			"zh-cn": "\\x0AmyColor的描述"
-		},
-		"htmlText": {
-			"en": "htmlText",
-			"de": "htmlText",
-			"ru": "htmlText",
-			"pt": "htmlText",
-			"nl": "htmlText",
-			"fr": "htmlText",
-			"it": "htmlText",
-			"es": "htmlText",
-			"pl": "htmlText",
-			"zh-cn": "htmlText"
-		},
-		"group_extraMyset": {
-			"en": "extraMyset",
-			"de": "extraMyset",
-			"ru": "extraMyset",
-			"pt": "extraMyset",
-			"nl": "extraMyset",
-			"fr": "extraMyset",
-			"it": "extraMyset",
-			"es": "extraMyset",
-			"pl": "extraMyset",
-			"zh-cn": "extraMyset"
-		},
-		"extraAttr": {
-			"en": "extraAttr",
-			"de": "extraAttr",
-			"ru": "extraAttr",
-			"pt": "extraAttr",
-			"nl": "extraAttr",
-			"fr": "extraAttr",
-			"it": "extraAttr",
-			"es": "extraAttr",
-			"pl": "extraAttr",
-			"zh-cn": "extraAttr"
-		}
-	});
-}
-
-// add translations for non-edit mode
-$.extend(true, systemDictionary, {
-	"Instance": {
-		"en": "Instance",
-		"de": "Instanz",
-		"ru": "Инстанция",
-		"pt": "Instância",
-		"nl": "Instantie",
-		"fr": "Instance",
-		"it": "Esempio",
-		"es": "Instancia",
-		"pl": "Instancja",
-		"zh-cn": "例"
-	}
+$.get( "adapter/${widgetName}/words.js", function(script) {
+	let translation = script.substring(script.indexOf('{'), script.length);
+	translation = translation.substring(0, translation.lastIndexOf(';'));
+	$.extend(systemDictionary, JSON.parse(translation));
 });
 
 // this code can be placed directly in ${widgetName}.html
