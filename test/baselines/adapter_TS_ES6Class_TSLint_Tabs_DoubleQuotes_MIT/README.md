929295bee81575adfe747b1517c526fe674c28f5
dominic.griesel <d.griesel@gmx.net>
2019-01-24 13:53:26 +0100

    Accept baselines

---
+++ test/baselines/adapter_TS_ES6Class_TSLint_Tabs_DoubleQuotes_MIT/README.md
@@ -0,0 +1,46 @@
+<h1>
+	<img src="admin/test-adapter.png" width="64"/>
+	ioBroker.test-adapter
+</h1>
+
+[![NPM version](http://img.shields.io/npm/v/iobroker.test-adapter.svg)](https://www.npmjs.com/package/iobroker.test-adapter)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.test-adapter.svg)](https://www.npmjs.com/package/iobroker.test-adapter)
+[![Dependency Status](https://img.shields.io/david/AlCalzone/iobroker.test-adapter.svg)](https://david-dm.org/AlCalzone/iobroker.test-adapter)
+[![Known Vulnerabilities](https://snyk.io/test/github/AlCalzone/ioBroker.test-adapter/badge.svg)](https://snyk.io/test/github/AlCalzone/ioBroker.test-adapter)
+
+[![NPM](https://nodei.co/npm/iobroker.test-adapter.png?downloads=true)](https://nodei.co/npm/iobroker.test-adapter/)
+
+**Tests:** Linux/Mac: [![Travis-CI](http://img.shields.io/travis/AlCalzone/ioBroker.test-adapter/master.svg)](https://travis-ci.org/AlCalzone/ioBroker.test-adapter)
+Windows: [![AppVeyor](https://ci.appveyor.com/api/projects/status/github/AlCalzone/ioBroker.test-adapter?branch=master&svg=true)](https://ci.appveyor.com/project/AlCalzone/ioBroker-test-adapter/)
+
+## test-adapter adapter for ioBroker
+
+Describe your project here
+
+## Changelog
+
+### 0.0.1
+* (Al Calzone) initial release
+
+## License
+MIT License
+
+Copyright (c) 2019 Al Calzone
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ test/baselines/adapter_TS_ES6Class_TSLint_Tabs_DoubleQuotes_MIT/src/main.ts
@@ -0,0 +1,155 @@
+/*
+ * Created with @iobroker/create-adapter v1.7.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+import * as utils from "@iobroker/adapter-core";
+
+// Load your modules here, e.g.:
+// import * as fs from "fs";
+
+// Augment the adapter.config object with the actual types
+// TODO: delete this in the next version
+declare global {
+	namespace ioBroker {
+		interface AdapterConfig {
+			// Define the shape of your options here (recommended)
+			option1: boolean;
+			option2: string;
+			// Or use a catch-all approach
+			[key: string]: any;
+		}
+	}
+}
+
+class TestAdapter extends utils.Adapter {
+
+	constructor(options: Partial<ioBroker.AdapterOptions> = {}) {
+		super({
+			...options,
+			name: "test-adapter",
+		});
+		this.on("ready", this.onReady);
+		this.on("objectChange", this.onObjectChange);
+		this.on("stateChange", this.onStateChange);
+		// this.on("message", this.onMessage);
+		this.on("unload", this.onUnload);
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	private async onReady() {
+		// Initialize your adapter here
+
+		// The adapters config (in the instance object everything under the attribute "native") is accessible via
+		// this.config:
+		this.log.info("config test1: " + this.config.option1);
+		this.log.info("config test1: " + this.config.option2);
+
+		/*
+		For every state in the system there has to be also an object of type state
+		Here a simple template for a boolean variable named "testVariable"
+		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+		*/
+		await this.setObjectAsync("testVariable", {
+			type: "state",
+			common: {
+				name: "testVariable",
+				type: "boolean",
+				role: "indicator",
+				read: true,
+				write: true,
+			},
+			native: {},
+		});
+
+		// in this template all states changes inside the adapters namespace are subscribed
+		this.subscribeStates("*");
+
+		/*
+		setState examples
+		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+		*/
+		// the variable testVariable is set to true as command (ack=false)
+		await this.setStateAsync("testVariable", true);
+
+		// same thing, but the value is flagged "ack"
+		// ack should be always set to true if the value is received from or acknowledged from the target system
+		await this.setStateAsync("testVariable", { val: true, ack: true });
+
+		// same thing, but the state is deleted after 30s (getState will return null afterwards)
+		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+
+		// examples for the checkPassword/checkGroup functions
+		let result = await this.checkPasswordAsync("admin", "iobroker");
+		this.log.info("check user admin pw ioboker: " + result);
+
+		result = await this.checkGroupAsync("admin", "admin");
+		this.log.info("check group user admin group admin: " + result);
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 */
+	private onUnload(callback: () => void) {
+		try {
+			this.log.info("cleaned everything up...");
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	/**
+	 * Is called if a subscribed object changes
+	 */
+	private onObjectChange(id: string, obj: ioBroker.Object | null | undefined) {
+		if (obj) {
+			// The object was changed
+			this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+		} else {
+			// The object was deleted
+			this.log.info(`object ${id} deleted`);
+		}
+	}
+
+	/**
+	 * Is called if a subscribed state changes
+	 */
+	private onStateChange(id: string, state: ioBroker.State | null | undefined) {
+		if (state) {
+			// The state was changed
+			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+		} else {
+			// The state was deleted
+			this.log.info(`state ${id} deleted`);
+		}
+	}
+
+	// /**
+	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+	//  * Using this method requires "common.message" property to be set to true in io-package.json
+	//  */
+	// private onMessage(obj: ioBroker.Message) {
+	// 	if (typeof obj === "object" && obj.message) {
+	// 		if (obj.command === "send") {
+	// 			// e.g. send email or pushover or whatever
+	// 			this.log.info("send command");
+
+	// 			// Send response in callback if required
+	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+	// 		}
+	// 	}
+	// }
+
+}
+
+if (module.parent) {
+	// Export the constructor in compact mode
+	module.exports = (options: Partial<ioBroker.AdapterOptions> | undefined) => new TestAdapter(options);
+} else {
+	// otherwise start the instance directly
+	(() => new TestAdapter())();
+}
