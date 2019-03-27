b2ffca26d21c495d026029867c18c98d9681d173
dominic.griesel <d.griesel@gmx.net>
2019-03-27 09:45:12 +0100

    Accept baselines

--- test/baselines/adapter_JS_ESLint_TypeChecking_Spaces_SingleQuotes_LGPLv3/main.js
+++ test/baselines/adapter_JS_ESLint_TypeChecking_Spaces_SingleQuotes_LGPLv3/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
- * Created with @iobroker/create-adapter v1.10.0
+ * Created with @iobroker/create-adapter v1.11.0
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
--- test/baselines/adapter_TS_ES6Class_TSLint_Tabs_DoubleQuotes_MIT/src/main.ts
+++ test/baselines/adapter_TS_ES6Class_TSLint_Tabs_DoubleQuotes_MIT/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v1.10.0
+ * Created with @iobroker/create-adapter v1.11.0
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
