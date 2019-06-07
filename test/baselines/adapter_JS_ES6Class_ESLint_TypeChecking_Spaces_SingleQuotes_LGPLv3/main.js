7ba6c7b4c9f7285cad709179bba38ff666957851
dominic.griesel <d.griesel@gmx.net>
2019-06-07 11:11:06 +0200

    Accept baselines

--- test/baselines/adapter_JS_ES6Class_ESLint_TypeChecking_Spaces_SingleQuotes_LGPLv3/main.js
+++ test/baselines/adapter_JS_ES6Class_ESLint_TypeChecking_Spaces_SingleQuotes_LGPLv3/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
- * Created with @iobroker/create-adapter v1.14.0
+ * Created with @iobroker/create-adapter v1.15.0
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
