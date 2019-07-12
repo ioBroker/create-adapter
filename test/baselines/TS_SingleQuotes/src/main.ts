584edf02ffe972755111a9fff5853e01714fa3aa
dominic.griesel <d.griesel@gmx.net>
2019-07-12 08:58:12 +0200

    accept baselines

--- test/baselines/TS_SingleQuotes/src/main.ts
+++ test/baselines/TS_SingleQuotes/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v1.15.0
+ * Created with @iobroker/create-adapter v1.15.1
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
