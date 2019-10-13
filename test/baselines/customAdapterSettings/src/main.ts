d6bcfc9c92af8cc2eed385a48d710dc047105a85
Dominic Griesel <d.griesel@gmx.net>
2019-10-13 18:17:18 +0200

    update baselines

--- test/baselines/customAdapterSettings/src/main.ts
+++ test/baselines/customAdapterSettings/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v1.16.0
+ * Created with @iobroker/create-adapter v1.17.0
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
