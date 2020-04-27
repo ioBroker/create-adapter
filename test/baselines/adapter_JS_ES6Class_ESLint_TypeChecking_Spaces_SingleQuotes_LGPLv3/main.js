af3453b912fe84c0b854efad122c04aa1fce43b7
Dominic Griesel <d.griesel@gmx.net>
2020-04-27 11:13:59 +0200

    update baselines [skip ci]

--- test/baselines/adapter_JS_ES6Class_ESLint_TypeChecking_Spaces_SingleQuotes_LGPLv3/main.js
+++ test/baselines/adapter_JS_ES6Class_ESLint_TypeChecking_Spaces_SingleQuotes_LGPLv3/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
- * Created with @iobroker/create-adapter v1.23.0
+ * Created with @iobroker/create-adapter v1.24.1
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
--- test/baselines/adapter_TS_ES6Class_ESLint_Tabs_DoubleQuotes_MIT/src/main.ts
+++ test/baselines/adapter_TS_ES6Class_ESLint_Tabs_DoubleQuotes_MIT/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v1.23.0
+ * Created with @iobroker/create-adapter v1.24.1
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
