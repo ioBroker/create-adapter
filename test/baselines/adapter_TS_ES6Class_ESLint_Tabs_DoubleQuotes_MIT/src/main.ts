fae548ea3a31a714341faac553c8331f057005bf
Dominic Griesel <d.griesel@gmx.net>
2020-05-04 08:00:26 +0200

    update baselines

--- test/baselines/adapter_TS_ES6Class_ESLint_Tabs_DoubleQuotes_MIT/src/main.ts
+++ test/baselines/adapter_TS_ES6Class_ESLint_Tabs_DoubleQuotes_MIT/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v1.24.1
+ * Created with @iobroker/create-adapter v1.24.2
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
