f4f47407113c9232b93da4dfd26c9f8082d5534c
Dominic Griesel <d.griesel@gmx.net>
2020-05-31 10:51:24 +0200

    accept baselines

--- test/baselines/connectionIndicator_yes/src/main.ts
+++ test/baselines/connectionIndicator_yes/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v1.24.2
+ * Created with @iobroker/create-adapter v1.25.0
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
