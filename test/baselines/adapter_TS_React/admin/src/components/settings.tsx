f49d6aec3ec69da515f26ba46a33756b54ff967a
Dominic Griesel <d.griesel@gmx.net>
2020-09-02 14:52:12 +0200

    create TS+React baselines, accept changes

---
+++ test/baselines/adapter_TS_React/admin/src/components/settings.tsx
@@ -0,0 +1,136 @@
+import * as React from "react";
+import { composeObject, entries } from "alcalzone-shared/objects";
+
+export type OnSettingsChangedCallback = (
+	newSettings: Record<string, unknown>,
+) => void;
+
+interface SettingsProps {
+	onChange: OnSettingsChangedCallback;
+	settings: Record<string, unknown>;
+}
+
+// Define here which settings exist and how they are typed
+interface SettingsState {
+	// catch-all for new and temporary values
+	[key: string]: unknown;
+	// they should all be optional, since they might not be set yet
+	// e.g. password?: string;
+}
+
+/** Example component for editing adapter settings */
+export class Settings extends React.Component<SettingsProps, SettingsState> {
+	constructor(props: SettingsProps) {
+		super(props);
+		// settings are our state
+		this.state = {
+			...props.settings,
+		};
+
+		// setup change handlers
+		this.handleChange = this.handleChange.bind(this);
+		// If you add your own, don't forget to bind them to `this`, e.g.:
+		// this.validateNetworkKey = this.validateNetworkKey.bind(this);
+	}
+
+	// MaterializeCSS checkboxed are messed up. To fix them, we need to...
+	// 1. store every reference to a checkbox in a variable
+	// private chkWriteLogFile: HTMLInputElement | null | undefined;
+	// 2. add the click event handler in `componentDidMount`
+	// if (this.chkWriteLogFile != null) {
+	// 	$(this.chkWriteLogFile).on("click", this.handleChange as any);
+	// }
+	// 3. remove the click handler in `componentWillUnmount`
+	// if (this.chkWriteLogFile != null) {
+	// 	$(this.chkWriteLogFile).off("click", this.handleChange as any);
+	// }
+
+	private parseChangedSetting(
+		target: HTMLInputElement | HTMLSelectElement,
+	): unknown {
+		// Checkboxes in MaterializeCSS are messed up, so we attach our own handler
+		// However that one gets called before the underlying checkbox is actually updated,
+		// so we need to invert the checked value here
+		return target.type === "checkbox"
+			? !(target as any).checked
+			: target.type === "number"
+			? parseInt(target.value, 10)
+			: target.value;
+	}
+
+	// gets called when the form elements are changed by the user
+	private handleChange(event: React.FormEvent<HTMLElement>) {
+		const target = event.target as HTMLInputElement | HTMLSelectElement; // TODO: more types
+		const value = this.parseChangedSetting(target);
+		return this.updateSettings(target.id, value);
+	}
+
+	private updateSettings(setting: string, value: unknown): boolean {
+		// store the setting
+		this.putSetting(setting, value, () => {
+			// and notify the admin UI about changes
+			this.props.onChange(
+				composeObject(
+					entries(this.state).filter(([k, v]) => !k.startsWith("_")),
+				),
+			);
+		});
+		return false;
+	}
+
+	/**
+	 * Reads a setting from the state object and transforms the value into the correct format
+	 * If no value is set, the default value is returned
+	 * @param key The setting key to lookup
+	 */
+	private getSetting(key: string, defaultValue?: unknown): unknown {
+		const ret = this.state[key];
+		return ret != undefined ? ret : defaultValue;
+	}
+
+	/**
+	 * Saves a setting in the state object and transforms the value into the correct format
+	 * @param key The setting key to store at
+	 */
+	private putSetting(
+		key: string,
+		value: unknown,
+		callback?: () => void,
+	): void {
+		this.setState({ [key]: value }, callback);
+	}
+
+	public componentDidMount() {
+		// update floating labels in materialize design
+		M.updateTextFields();
+
+		// Add checkbox click handlers here
+	}
+
+	public componentWillUnmount() {
+		// Remove checkbox click handlers here
+	}
+
+	public componentDidUpdate() {
+		// update floating labels in materialize design
+		M.updateTextFields();
+	}
+
+	public render() {
+		return (
+			<>
+				<div className="row">
+			<div className="col s6 input-field">
+				<input type="checkbox" className="value" id="option1" />
+				<label htmlFor="option1" className="translate">option1</label>
+			</div>
+
+			<div className="col s6 input-field">
+				<input type="text" className="value" id="option2" />
+				<label htmlFor="option2" className="translate">option2</label>
+			</div>
+				</div>
+			</>
+		);
+	}
+}
---
+++ test/baselines/adapter_TS_React/admin/src/index.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { OnSettingsChangedCallback, Settings } from "./components/settings";
+import {
+	subscribeStatesAsync,
+	subscribeObjectsAsync,
+	unsubscribeStatesAsync,
+	unsubscribeObjectsAsync,
+} from "./lib/backend";
+
+/** The namespace of this adapter */
+let namespace: string;
+/** The selector to subscribe to system state changes (e.g. adapter alive) */
+let systemStates: string;
+/** The selector to subscribe to all of the adaper's own states */
+let adapterStates: string;
+
+interface RootProps {
+	settings: Record<string, unknown>;
+	onSettingsChanged: OnSettingsChangedCallback;
+}
+
+/** The root component displays all components that belong to the adapter configuration */
+function Root(props: RootProps) {
+	// Subscribe and unsubscribe from states and objects
+	React.useEffect(() => {
+		namespace = `${adapter}.${instance}`;
+		// subscribe to changes
+		systemStates = `system.adapter.${namespace}.*`;
+		adapterStates = `${namespace}.*`;
+		window.addEventListener("unload", () => onUnload());
+
+		// If you need to subscribe to some of these states, uncomment the corresponding line
+		// Alternatively, subscribe to specific states instead of all of them.
+
+		// void subscribeStatesAsync(systemStates);
+		// void subscribeObjectsAsync(adapterStates);
+		// void subscribeStatesAsync(adapterStates);
+
+		return onUnload;
+	}, []);
+
+	function onUnload() {
+		// If you subscribed to some of the states above, don't forget to unsubscribe here!
+		// void unsubscribeStatesAsync(systemStates);
+		// void unsubscribeObjectsAsync(adapterStates);
+		// void unsubscribeStatesAsync(adapterStates);
+	}
+
+	return (
+		/* Render additional components, e.g. tab views here */
+		<Settings
+			settings={props.settings}
+			onChange={props.onSettingsChanged}
+		/>
+	);
+}
+
+// Used to compare the previously saved setting with the current configuration
+let curSettings: Record<string, unknown>;
+let originalSettings: Record<string, unknown>;
+
+/**
+ * Checks if any setting was changed
+ */
+function hasChanges(): boolean {
+	if (
+		Object.keys(originalSettings).length !== Object.keys(curSettings).length
+	)
+		return true;
+	for (const key of Object.keys(originalSettings)) {
+		if (originalSettings[key] !== curSettings[key]) return true;
+	}
+	return false;
+}
+
+// When the config page is loaded, set up the settings change handler and render the root component
+(window as any).load = (
+	settings: Record<string, unknown>,
+	onChange: (hasChanges: boolean) => void,
+) => {
+	originalSettings = settings;
+
+	const settingsChanged: OnSettingsChangedCallback = (newSettings) => {
+		curSettings = newSettings;
+		onChange(hasChanges());
+	};
+
+	ReactDOM.render(
+		<Root settings={settings} onSettingsChanged={settingsChanged} />,
+		document.getElementById("adapter-container") ||
+			document.getElementsByClassName("adapter-container")[0],
+	);
+
+	// Disable the save buttons because nothing was changed yet
+	onChange(false);
+};
+
+// When the save button is clicked, overwrite the original settings variable with the new settings
+(window as any).save = (
+	callback: (newSettings: Record<string, unknown>) => void,
+) => {
+	// save the settings
+	callback(curSettings);
+	originalSettings = curSettings;
+};
