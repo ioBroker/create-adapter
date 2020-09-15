51eb7f4eaeb8be871f0a2ab0a87d1bc2a63a4dca
Dominic Griesel <d.griesel@gmx.net>
2020-09-15 09:20:56 +0200

    select GitHub Actions as the default CI, make choice exclusive

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -597,12 +597,12 @@ export const questionsAndText: (
 					"The Unlicense",
 				],
 			},
-			styledMultiselect({
+			{
+				type: "select",
 				name: "ci",
 				expert: true,
 				message: "Which continuous integration service should be used?",
-				initial: ((answers: Answers) =>
-					answers.features.includes("adapter") ? [1] : [0]) as any,
+				initial: 0,
 				choices: [
 					{
 						message: "GitHub Actions",
@@ -613,7 +613,7 @@ export const questionsAndText: (
 						value: "travis",
 					},
 				],
-			}),
+			},
 			{
 				type: "select",
 				name: "dependabot",
