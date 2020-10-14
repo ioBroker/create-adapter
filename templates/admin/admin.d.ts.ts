9ea4cddecdc62f41727b0a6fc786a5732ec3c9a4
Samuel Weibel <samuel.weibel@gmail.com>
2020-10-14 17:16:55 +0200

    admin.d.ts is no longer required for a react adapter (everything is done through Connection).

--- templates/admin/admin.d.ts.ts
+++ templates/admin/admin.d.ts.ts
@@ -3,11 +3,11 @@ import { readFile, TemplateFunction } from "../../src/lib/createAdapter";
 const templateFunction: TemplateFunction = answers => {
 
 	const useTypeScript = answers.language === "TypeScript";
+	const useReact = answers.adminReact === "yes";
 	const useTypeChecking = answers.tools && answers.tools.indexOf("type checking") > -1;
-	if (!useTypeScript && !useTypeChecking) return;
+	if ((useTypeScript && useReact) || (!useTypeScript && !useTypeChecking)) return;
 
 	return readFile("admin.raw.d.ts", __dirname);
 };
 
-templateFunction.customPath = answers => answers.adminReact === "yes" ? "admin/src/lib/admin.d.ts" : "admin/admin.d.ts";
 export = templateFunction;
