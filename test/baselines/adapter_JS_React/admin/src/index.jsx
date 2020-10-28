9d7d89fe4f9825f4cdfd39abf16e163a1965079d
Dominic Griesel <d.griesel@gmx.net>
2020-10-28 13:55:30 +0000

    accept baselines

--- test/baselines/adapter_JS_React/admin/src/index.jsx
+++ test/baselines/adapter_JS_React/admin/src/index.jsx
@@ -5,13 +5,13 @@ import theme from "@iobroker/adapter-react/Theme";
 import Utils from "@iobroker/adapter-react/Components/Utils";
 import App from "./app";
 
-window["adapterName"] = "test-adapter";
 let themeName = Utils.getThemeName();
 
 function build() {
 	ReactDOM.render(
 		<MuiThemeProvider theme={theme(themeName)}>
 			<App
+				adapterName="test-adapter"
 				onThemeChange={(_theme) => {
 					themeName = _theme;
 					build();
