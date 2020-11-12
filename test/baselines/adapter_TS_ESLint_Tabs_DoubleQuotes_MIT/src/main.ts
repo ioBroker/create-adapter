a64a05d0dfc1143871e617835fad54e15dd781be
Dominic Griesel <d.griesel@gmx.net>
2020-11-12 17:17:23 +0100

    accept baselines

--- test/baselines/adapter_TS_ESLint_Tabs_DoubleQuotes_MIT/src/main.ts
+++ test/baselines/adapter_TS_ESLint_Tabs_DoubleQuotes_MIT/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v1.30.0
+ * Created with @iobroker/create-adapter v1.30.1
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
