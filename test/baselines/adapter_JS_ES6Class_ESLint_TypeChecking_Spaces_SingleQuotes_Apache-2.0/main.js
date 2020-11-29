88500c3da17d80b7d18ce801f89dbcfc3d4d453e
Samuel Weibel <samuel.weibel@gmail.com>
2020-11-29 14:10:05 +0100

    accept baselines

--- test/baselines/adapter_JS_ES6Class_ESLint_TypeChecking_Spaces_SingleQuotes_Apache-2.0/main.js
+++ test/baselines/adapter_JS_ES6Class_ESLint_TypeChecking_Spaces_SingleQuotes_Apache-2.0/main.js
@@ -138,7 +138,7 @@ class TestAdapter extends utils.Adapter {
     // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
     // /**
     //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.message" property to be set to true in io-package.json
+    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
     //  * @param {ioBroker.Message} obj
     //  */
     // onMessage(obj) {
