4d4e37271e2a2dffab811b3ecd013518c75aaf0d
Dominic Griesel <d.griesel@gmx.net>
2021-03-01 13:44:45 +0100

    fix: remove obsolete prettier/@typescript-eslint config

--- templates/_eslintrc_typescript.js.ts
+++ templates/_eslintrc_typescript.js.ts
@@ -25,7 +25,6 @@ module.exports = {
 	extends: [
 		"plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
 ${usePrettier ? (`
-		"prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
 		"plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 `) : ""}${useReact ? (`
 		"plugin:react/recommended", // Supports React JSX
