9f8361aaa937d637046564b914f99a965be39af5
Dominic Griesel <d.griesel@gmx.net>
2021-09-06 09:33:53 +0200

    Allow exitCode 11 by default for schedule adapters during testing

--- templates/test/integration.js.ts
+++ templates/test/integration.js.ts
@@ -1,9 +1,26 @@
-import { readFile, TemplateFunction } from "../../src/lib/createAdapter";
+import { TemplateFunction } from "../../src/lib/createAdapter";
 
-export = (answers => {
+const templateFunction: TemplateFunction = answers => {
 
 	const isAdapter = answers.features.indexOf("adapter") > -1;
 	if (!isAdapter) return;
 
-	return readFile("integration.raw.js", __dirname);
-}) as TemplateFunction;
+	const isScheduleAdapter = answers.startMode === "schedule";
+
+	const template = `
+const path = require("path");
+const { tests } = require("@iobroker/testing");
+
+// Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options${
+	isScheduleAdapter ? (`
+tests.integration(path.join(__dirname, ".."), {
+	allowedExitCodes: [11],
+});
+	`) : (`
+tests.integration(path.join(__dirname, ".."));
+	`)}
+`;
+	return template.trim();
+};
+
+export = templateFunction;
