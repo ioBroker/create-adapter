45bbed61ac6892f9d8e304ee5fd8b5353c582752
Samuel Weibel <samuel.weibel@gmail.com>
2021-09-08 17:40:42 +0200

    Updated alt texts of ioBroker badges. Fixes #722

--- templates/README.md.ts
+++ templates/README.md.ts
@@ -62,8 +62,8 @@ export = (answers => {
 
 [![NPM version](https://img.shields.io/npm/v/iobroker.${adapterNameLowerCase}.svg)](https://www.npmjs.com/package/iobroker.${adapterNameLowerCase})
 [![Downloads](https://img.shields.io/npm/dm/iobroker.${adapterNameLowerCase}.svg)](https://www.npmjs.com/package/iobroker.${adapterNameLowerCase})
-![Number of Installations (latest)](https://iobroker.live/badges/${adapterNameLowerCase}-installed.svg)
-![Number of Installations (stable)](https://iobroker.live/badges/${adapterNameLowerCase}-stable.svg)
+![Number of Installations](https://iobroker.live/badges/${adapterNameLowerCase}-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/${adapterNameLowerCase}-stable.svg)
 [![Dependency Status](https://img.shields.io/david/${answers.authorGithub}/iobroker.${adapterNameLowerCase}.svg)](https://david-dm.org/${answers.authorGithub}/iobroker.${adapterNameLowerCase})
 
 [![NPM](https://nodei.co/npm/iobroker.${adapterNameLowerCase}.png?downloads=true)](https://nodei.co/npm/iobroker.${adapterNameLowerCase}/)
