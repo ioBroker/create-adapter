8a1bc1d6301f2fe21a84f374e825522e237fc3b4
Dominic Griesel <d.griesel@gmx.net>
2021-10-17 23:59:34 +0200

    accept baselines

---
+++ test/baselines/adapter_JS_React/.build.js
@@ -0,0 +1,30 @@
+/** Build script to use esbuild without specifying 1000 CLI options */
+const { build, cliopts } = require("estrella");
+const glob = require("tiny-glob");
+
+const [opts, args] = cliopts.parse(
+	["react", "Build React sources"],
+);
+
+if (opts.react) {
+	(async () => {
+		const entryPoints = await glob("./admin/src/{index,tab}.{jsx,tsx}");
+		await build({
+			entryPoints,
+			tsconfig: "./admin/tsconfig.json",
+			bundle: true,
+			splitting: true,
+			format: "esm",
+			minify: !cliopts.watch,
+			outdir: "admin/build",
+			sourcemap: true,
+			// sourcesContent: true,
+			logLevel: "info",
+			define: {
+				"process.env.NODE_ENV": cliopts.watch
+					? '"development"'
+					: '"production"',
+			},
+		});
+	})().catch(() => process.exit(1));
+}
