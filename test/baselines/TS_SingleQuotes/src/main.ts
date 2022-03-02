f8d33fe432fe42d1cb71572d74cb6cd1cfb8eac0
Samuel Weibel <UncleSamSwiss@users.noreply.github.com>
2022-03-02 21:50:01 +0100

    Set eraseOnUpload to true for React adapters (#886)
    Co-authored-by: AlCalzone <d.griesel@gmx.net>
    Co-authored-by: Samuel Weibel <samuel.weibel@sensioty.ch>

--- test/baselines/TS_SingleQuotes/src/main.ts
+++ test/baselines/TS_SingleQuotes/src/main.ts
@@ -1,5 +1,5 @@
 /*
- * Created with @iobroker/create-adapter v2.0.2
+ * Created with @iobroker/create-adapter v2.1.0
  */
 
 // The adapter-core module gives you access to the core ioBroker functions
