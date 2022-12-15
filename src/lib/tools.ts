2e41d6fa1f6f3bf1a09ee758c26a3bd960db8693
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-12-15 23:31:14 +0100

    fix type error properly

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,4 +1,4 @@
-import { isArray, isObject } from "alcalzone-shared/typeguards";
+import { isObject } from "alcalzone-shared/typeguards";
 import { bold } from "ansi-colors";
 import type { AxiosRequestConfig } from "axios";
 import { spawn, SpawnOptions } from "child_process";
@@ -65,8 +65,8 @@ export function executeCommand(
 ): Promise<ExecuteCommandResult> {
 	return new Promise((resolve) => {
 		let args: string[] | undefined;
-		if (isArray(argsOrOptions)) {
-			args = argsOrOptions as string[];
+		if (Array.isArray(argsOrOptions)) {
+			args = argsOrOptions;
 		} else if (isObject(argsOrOptions)) {
 			// no args were given
 			options = argsOrOptions;
