17045c5b43a367617bffca939d7a33e66e8180b7
Martin M <mcm57@gmx.at>
2023-05-07 11:12:34 +0200

    Update node versions to 16.x/18.x/20.x

--- templates/_github/workflows/test-and-release.yml.ts
+++ templates/_github/workflows/test-and-release.yml.ts
@@ -14,8 +14,8 @@ const templateFunction: TemplateFunction = answers => {
 	const useReleaseScript = answers.releaseScript === "yes";
 	const isGitHub = answers.target === "github";
 
-	const latestNodeVersion = "16.x";
-	const adapterTestVersions = ["14.x", "16.x", "18.x"];
+	const latestNodeVersion = "18.x";
+	const adapterTestVersions = ["16.x", "18.x", "20.x"];
 	const adapterTestOS = ["ubuntu-latest", "windows-latest", "macos-latest"];
 
 	const adapterName = answers.adapterName;
